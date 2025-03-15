bededb396deeb165944c1c019e037802db34c624
willymariino <wg.mariino@gmail.com>
2025-03-15 14:03:35 +0100

    changed javascript

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // inserisco il server da cui vado a prendere le email
-const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/S"
 
 // indico dove voglio andare a posizionare le email
-const emailContainer = document.getElementById("email-container")
-const emailList = document.getElementById("email-list")
+const emailContainer = document.querySelector("polaroid-grid")
+const emailList = document.querySelector("polaroid")
 
 const emailCount = 10
 
